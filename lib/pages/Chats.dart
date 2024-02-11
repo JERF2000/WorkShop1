d75c8f3f359c5217b74ada8d87ddbc9f1a032cfe
Jasir Reyes <esaud278@gmail.com>
2024-02-11 11:13:01 -0600

    Se agrego navegación por Tabs y se creo la clase de chats

---
+++ lib/pages/Chats.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Chats extends StatelessWidget{
+  const Chats ({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Pagina de Chats"),
+    );
+  }
+
+}
--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:navegacion_tabs/pages/Chats.dart';
+import 'package:navegacion_tabs/pages/Novedades.dart';
+import 'package:navegacion_tabs/pages/Llamadas.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({
@@ -7,9 +10,35 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Hello World!'),
+    //DefaultTabController -> Para crear navegación por Tabs
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        //Creación de Navegación por Tabs (Pestañas del AppBar)
+        appBar: AppBar(
+          title: Text("Chat's App"),
+          bottom: TabBar(
+            tabs: [
+              Tab(
+                icon: Icon(Icons.chat),
+                child: Text("Chats"),
+                ),
+              Tab(
+                icon: Icon(Icons.newspaper_rounded),
+                child: Text("Novedades"),
+                ),
+              Tab(
+                icon: Icon(Icons.call),
+                child: Text("Llamadas"),
+                ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Chats(),
+          ],
+        ),
       ),
     );
   }
